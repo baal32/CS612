315b013330e0685882d44bfbc6555fb756976576
Noah Gilbert <baal32@hotmail.com>
2015-03-07 14:30:41 -0800

    HW2 complete

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -7,6 +7,7 @@ class HW2:
         self.columns = None
         self.rows = None
         self.sliced_matrix = None
+        self.normalized_matrix = None
 
 
     def create_TenXTen(self):
@@ -18,29 +19,33 @@ class HW2:
     def read_TenXTen(self):
         self.TenXTen = np.loadtxt("tenxten.txt",dtype=np.int32)
 
-    def read_rows(self):
-        self.rows = np.loadtxt("rows.txt", dtype=np.int32)
+    def read_rows(self, fname):
+        self.rows = np.loadtxt(fname, dtype=np.int32)
+        if self.rows.min() < 1 or self.rows.max() > self.TenXTen.shape[0]:
+            raise ValueError('Rows are out of bounds')
 
-    def read_cols(self):
-        self.columns=np.loadtxt("columns.txt",dtype=np.int32)
+    def read_cols(self, fname):
+        self.columns=np.loadtxt(fname,dtype=np.int32)
+        if self.columns.min() < 1 or self.columns.max() > self.TenXTen.shape[1]:
+            raise ValueError('Columns are out of bounds')
 
     def slice_TenXTen(self):
         self.sliced_matrix = self.TenXTen[self.rows[:,None],self.columns] # this is necessary as self.rows, self.columns only returns intersections - https://stackoverflow.com/questions/22927181/selecting-specific-rows-and-columns-from-numpy-array
 
-    def print_sliced_matrix(self):
-        print self.sliced_matrix
-
     def normalize(self):
-        pass
+        maxVal = self.TenXTen.max()
+        minVal = self.TenXTen.min()
+        self.normalized_matrix = (self.TenXTen - minVal)/float((maxVal-minVal)) # note that float() is necessary to avoid integer division
+
 
     def find_mean(self):
-        pass
+        return np.mean(self.TenXTen)
 
     def find_median(self):
-        pass
+        return np.median(self.TenXTen)
 
     def find_std(self):
-        pass
+        return np.std(self.TenXTen)
 
 
 if __name__ == '__main__': # note this allows us to include this file in the test_HW2.py without trying to evaluate the code below - this is important to prevent errors below (as in functions called but not yet implemented)
@@ -48,12 +53,13 @@ if __name__ == '__main__': # note this allows us to include this file in the tes
     TenXTen = hw2.create_TenXTen()
     hw2.write_to_file(TenXTen)
     hw2.read_TenXTen()
+    print "Full Matrix **************\n", hw2.TenXTen
     hw2.read_rows()
     hw2.read_cols()
     hw2.slice_TenXTen()
-    hw2.print_sliced_matrix()
+    print "Sliced Matrix *********\n", hw2.sliced_matrix
     hw2.normalize()
-    hw2.find_mean()
-    hw2.find_median()
-    hw2.find_std()
-    hw2.dosomethingfake()
+    print ("Normalized Matrix ****************\n",hw2.normalized_matrix)
+    print "Mean**********\n", hw2.find_mean()
+    print "Median*********\n",hw2.find_median()
+    print "Standard Deviation********\n",hw2.find_std()
--- HW2/test_HW2.py
+++ HW2/test_HW2.py
@@ -1,9 +1,12 @@
 import unittest
+from unittest import TestCase
 from HW2 import HW2
 import numpy as np
 
-__author__ = 'John'
-
+__author__ = 'ngilbert'
+# Arrange:
+# Act:
+# Assert:
 
 class TestHW2(unittest.TestCase):
     def test_create_TenXTen(self):
@@ -12,7 +15,7 @@ class TestHW2(unittest.TestCase):
         # Act: Call create_TenXTen on the HW2 object and assign resulting matrix to TenXTen
         hw.create_TenXTen()
         TenXTen = hw.TenXTen
-        #Assert: Verify that the result is size 10 x 10
+        # Assert: Verify that the result is size 10 x 10
         self.assertEqual(np.shape(TenXTen), (10, 10))
 
     def test_slice_TenXTen(self):
@@ -20,12 +23,69 @@ class TestHW2(unittest.TestCase):
         hw2 = HW2()
         # Act: Create a TenXTen matrix, slice it and set the results to sliced
         hw2.create_TenXTen()
-        hw2.read_cols()
-        hw2.read_rows()
+        hw2.read_cols("columns.txt")
+        hw2.read_rows("rows.txt")
         hw2.slice_TenXTen()
         sliced = hw2.sliced_matrix
         # Assert: Assert that the size of sliced is 4x4
-        self.assertEqual(np.shape(sliced),(4,4))
+        self.assertEqual(np.shape(sliced), (4, 4))
+
+    def test_normalize(self):
+        # Arrange: create a new hw2 object
+        hw2 = HW2()
+        #Act: Call reate_TenXTen and normalize
+        hw2.create_TenXTen()
+        hw2.normalize()
+        normalized = hw2.normalized_matrix
+        #Assert: Assert that the max value of the normalized matrix is < 1
+        self.assertLessEqual(normalized.max(), 1)
+
+    def test_read_rows(self):
+        # Arrange:  create a new hw2 object
+        hw2 = HW2()
+        hw2.create_TenXTen()
+        # Act: Read the row indices from rows.txt
+        hw2.read_rows("rows.txt")
+        rows = hw2.rows
+        # Assert:
+        self.assertEqual(rows.size,4)
+
+    def test_read_cols(self):
+        # Arrange: create a new hw2 object
+        hw2 = HW2()
+        hw2.create_TenXTen()
+        # Act: read the column indices from columns.txt
+        hw2.read_cols("columns.txt")
+        columns = hw2.columns
+        # Assert:
+        self.assertEqual(columns.size,4)
+
+    def test_find_mean(self):
+        # Arrange: create a new hw2 object
+        hw2 = HW2()
+        hw2.create_TenXTen()
+        # Act: call find_mean and assign value to mean
+        mean = hw2.find_mean()
+        # Assert: assert that mean is 49.5
+        self.assertEqual(mean,49.5)
+
+    def test_find_median(self):
+        # Arrange: create a new hw2 object
+        hw2 = HW2()
+        hw2.create_TenXTen()
+        # Act: call find_median and assign value to median
+        median = hw2.find_median()
+        # Assert: assert that mean is 49.5
+        self.assertEqual(median,49.5)
+
+    def test_find_std(self):
+        # Arrange: create a new hw2 object
+        hw2 = HW2()
+        hw2.create_TenXTen()
+        # Act: call find_std and assign value to std
+        std = hw2.find_std()
+        # Assert: assert that mean is 49.5
+        self.assertEqual("{0:.6f}".format(std),"{0:.6f}".format(28.8660700477))
 
 if __name__ == '__main__':
     unittest.main()
